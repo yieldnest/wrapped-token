bf6f22dbf2ca05a9a31fd41e7731bbfc5d1dceb6
danoctavian <danoctavian91@gmail.com>
2025-04-28 05:12:43 +0300

    add GovernedWrappedToken

---
+++ src/GovernedWrappedToken.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {WrappedToken} from "./WrappedToken.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title GovernedWrappedToken
+ * @dev An extension of WrappedToken that allows authorized allocators to transfer
+ * underlying tokens out of the contract for management purposes.
+ */
+contract GovernedWrappedToken is WrappedToken {
+    /**
+     * @dev Role for governors who can manage token allocations
+     */
+    bytes32 public constant GOVERNOR_ROLE = keccak256("GOVERNOR_ROLE");
+
+    /**
+     * @dev Emitted when a governor transfers underlying tokens out of the contract.
+     */
+    event UnderlyingWithdrawn(address indexed allocator, address indexed recipient, uint256 amount);
+
+    /**
+     * @dev Transfers a specified amount of the underlying token to a recipient.
+     * Can only be called by accounts with the governor role.
+     * @param recipient The address that will receive the tokens
+     * @param amount The amount of underlying tokens to transfer
+     */
+    function withdrawUnderlying(address recipient, uint256 amount) external onlyRole(GOVERNOR_ROLE) {
+        require(recipient != address(0), "Cannot allocate to zero address");
+
+        // Get current backing information
+        (uint256 totalWrappedInUnderlying, uint256 actualUnderlying) = backing();
+
+        // Ensure there are enough excess tokens to allocate
+        require(actualUnderlying >= totalWrappedInUnderlying, "Insufficient backing");
+        uint256 excessTokens = actualUnderlying - totalWrappedInUnderlying;
+        require(amount <= excessTokens, "Allocation exceeds available excess");
+
+        // Transfer the tokens to the recipient
+        SafeERC20.safeTransfer(IERC20(asset()), recipient, amount);
+
+        emit UnderlyingWithdrawn(msg.sender, recipient, amount);
+    }
+}
